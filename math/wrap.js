11dfeb18aa84d4ee72cf86a80647551d0d5c367b
bpetryshyn <bpetryshyn@hubspot.com>
2020-11-01 17:14:23 +0200

    Use real 16 bit variables to store values

--- math/wrap.js
+++ math/wrap.js
@@ -1,5 +1,10 @@
 const wrap = (number, wordSize) => {
-    return number % (2 ** wordSize);
+    switch (wordSize) {
+        case 16:
+            return new Uint16Array([number])[0];
+        case 32:
+            return new Uint32Array([number])[0];
+    }
 };
 
 module.exports = wrap;
