84758bfd1b638993b8ae4610c55bef9a7f056e4b
bpetryshyn <bpetryshyn@hubspot.com>
2020-11-01 16:01:19 +0200

    Remove sign before writing result to buffer

--- crypto/writeWord.js
+++ crypto/writeWord.js
@@ -1,9 +1,9 @@
 const writeWord = (buffer, value, position, wordSize) => {
     switch (wordSize) {
         case 16:
-            return buffer.writeUInt16LE(value, position);
+            return buffer.writeUInt16LE(value >>> 0, position);
         case 32:
-            return buffer.writeUInt32LE(value,position);
+            return buffer.writeUInt32LE(value >>> 0,position);
     }
 }
 
