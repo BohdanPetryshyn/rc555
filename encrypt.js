ccc211c9e9e1c1e5eb8e535dafba5892548079ea
bpetryshyn <bpetryshyn@hubspot.com>
2020-11-05 21:00:56 +0200

    Add encryption command and main executable rc555 file

---
+++ encrypt.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+const StreamEncryptor = require('./crypto/StreamEncryptor');
+
+const getDefaultOutputFileName = inputFileName => inputFileName + '.rc555';
+
+const encrypt = ({
+    argv,
+    keySize,
+    rounds,
+    wordSize
+}) => {
+    const password = argv['p'];
+    const inputFileName = argv._[1];
+    const outputFileName = argv['o'] || getDefaultOutputFileName(inputFileName);
+
+    fs.createReadStream(inputFileName)
+        .pipe(new StreamEncryptor(password, keySize, rounds, wordSize))
+        .pipe(fs.createWriteStream(outputFileName));
+};
+
+module.exports = encrypt;
---
+++ rc555.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const parseArgs = require('minimist');
+
+const encrypt = require('./encrypt');
+
+const argv = parseArgs(process.argv.slice(2));
+
+const command = argv._[0];
+
+const keySize = argv['b'] || 128;
+const rounds = argv['r'] || 128;
+const wordSize = argv['w'] || 32;
+
+switch (command) {
+    case 'encrypt':
+        encrypt({argv, keySize, rounds, wordSize});
+        break;
+    default:
+        console.log(`Unknown command "${command}".`);
+}
