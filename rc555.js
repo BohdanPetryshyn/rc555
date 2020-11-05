5d257a94bb7b159cf8953169c5fd3a9ea0848255
bpetryshyn <bpetryshyn@hubspot.com>
2020-11-05 21:40:00 +0200

    Validate word size

--- rc555.js
+++ rc555.js
@@ -5,6 +5,8 @@ const parseArgs = require('minimist');
 const encrypt = require('./encrypt');
 const decrypt = require('./decrypt');
 
+const ALLOWED_WORD_SIZES = [16, 32];
+
 const argv = parseArgs(process.argv.slice(2));
 
 const command = argv._[0];
@@ -12,6 +14,13 @@ const command = argv._[0];
 const keySize = argv['b'] || 128;
 const rounds = argv['r'] || 128;
 const wordSize = argv['w'] || 32;
+
+if (!ALLOWED_WORD_SIZES.includes(wordSize)) {
+  console.error(
+    `Invalid wordSize "${wordSize}". Allowed word sizes: ${ALLOWED_WORD_SIZES}`
+  );
+  process.exit(1);
+}
 
 switch (command) {
   case 'encrypt':
