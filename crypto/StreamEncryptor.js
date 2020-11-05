dc7dbbd6a06e3c846f3e3253ed831326cd9f6e7d
bpetryshyn <bpetryshyn@hubspot.com>
2020-11-05 12:11:24 +0200

    Implement StreamDecryptor

--- crypto/StreamEncryptor.js
+++ crypto/StreamEncryptor.js
@@ -2,11 +2,13 @@ const {Transform} = require('stream');
 
 const xor = require('buffer-xor');
 
-const hash = require('../utils/hash');
 const getRandomBuffer = require('../utils/getRandomBuffer');
 const toChunks = require('../utils/toChunks');
 
 const BlockCryptor = require('./blockcryptor/BlockCryptor');
+const createKey = require('./createKey');
+
+const EMPTY_BUFFER = Buffer.alloc(0);
 
 class StreamEncryptor extends Transform {
     constructor(password, keySize, rounds, wordSize) {
@@ -15,13 +17,13 @@ class StreamEncryptor extends Transform {
         this.blockSize = wordSizeBytes * 2;
 
         this.blockCryptor = new BlockCryptor(
-            this.getKey(password, keySize),
+            createKey(password, keySize),
             rounds,
             wordSize
         );
 
         this.lastBlockCipher = this.getInitBlock();
-        this.lastSubBlock = Buffer.alloc(0);
+        this.lastSubBlock = EMPTY_BUFFER;
     }
 
     _transform(chunk, encoding, callback) {
@@ -29,7 +31,7 @@ class StreamEncryptor extends Transform {
 
         const withLastSubBlock = Buffer.concat([chunk, this.lastSubBlock]);
         const blocks = toChunks(withLastSubBlock, this.blockSize);
-        this.lastSubBlock = blocks.pop();
+        this.lastSubBlock = blocks.pop() || EMPTY_BUFFER;
 
         blocks.forEach(this.processBlock);
 
@@ -71,9 +73,9 @@ class StreamEncryptor extends Transform {
     writeInitBlockIfFirstTransform() {
         if (this.isFirstTransform()) {
             const initBlock = this.lastBlockCipher;
-
+            const encryptedInitBlock = this.blockCryptor.encrypt(initBlock);
             this.push(
-                this.blockCryptor.encrypt(initBlock)
+                encryptedInitBlock
             );
         }
     }
@@ -89,14 +91,6 @@ class StreamEncryptor extends Transform {
 
     getInitBlock() {
         return getRandomBuffer(this.blockSize);
-    }
-
-    getKey(password, keySize) {
-        const passwordHash = hash(password);
-
-        return keySize < password.length
-            ? passwordHash.slice(0, keySize)
-            : Buffer.concat([passwordHash, hash(passwordHash)], keySize)
     }
 }
 
---
+++ crypto/createKey.js
@@ -0,0 +1,11 @@
+const hash = require('../utils/hash');
+
+const createKey = (password, keySize) => {
+    const passwordHash = hash(password);
+
+    return keySize < password.length
+        ? passwordHash.slice(0, keySize)
+        : Buffer.concat([passwordHash, hash(passwordHash)], keySize)
+}
+
+module.exports = createKey;
